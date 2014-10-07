0c861ca677be333bdabb1a154438adfa2a88c16e
Francis Tseng <f@frnsys.com>
2014-10-07 02:39:52 +0000

    flud=>glimpse

--- setup/setup.sh
+++ setup/setup.sh
@@ -6,28 +6,28 @@ sudo apt-get install nginx python3-pip git uwsgi supervisor uwsgi-plugin-python 
 
 # Virtualenv
 pip3 install virtualenv
-virtualenv -p python2.7 /env/flud --no-site-packages
+virtualenv -p python2.7 /env/glimpse --no-site-packages
 
 # Sass
 gem install sass
 
 # Setup app
-git clone https://github.com/publicscience/flud.git /srv/flud
-cd /srv/flud
+git clone https://github.com/publicscience/glimpse.git /srv/glimpse
+cd /srv/glimpse
 mv config-sample.py config.py
-source /env/flud/bin/activate
+source /env/glimpse/bin/activate
 pip install -r requirements.txt
 
 # Supervisor
-sudo cp /srv/flud/setup/supervisor/*.conf /etc/supervisor/conf.d/
+sudo cp /srv/glimpse/setup/supervisor/*.conf /etc/supervisor/conf.d/
 
 # Nginx
 sudo sed -i '' 's/# server_names_hash_bucket_size/server_names_hash_bucket_size/g' /etc/nginx/nginx.conf
-sudo cp /srv/flud/setup/nginx/*.conf /etc/nginx/conf.d/
+sudo cp /srv/glimpse/setup/nginx/*.conf /etc/nginx/conf.d/
 
 # Restart
 sudo service nginx restart
 sudo service supervisor restart
 
 # Warning
-echo -e "\n\nYou should edit /srv/flud/config.py for your settings, then restart supervisor.\n\n"
+echo -e "\n\nYou should edit /srv/glimpse/config.py for your settings, then restart supervisor.\n\n"
